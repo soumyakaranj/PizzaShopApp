4f3c2fa7f60bc81e754971e67b73eb4ac72a2590
Vishal <vrbiyani@gmail.com>
2014-12-18 20:47:42 +0530

    Update PizzaOrderService.java

--- src/main/java/com/springsource/pizzashop/service/PizzaOrderService.java
+++ src/main/java/com/springsource/pizzashop/service/PizzaOrderService.java
@@ -4,5 +4,4 @@ import org.springframework.roo.addon.layers.service.RooService;
 @RooService(domainTypes = { com.springsource.pizzashop.domain.PizzaOrder.class })
 public interface PizzaOrderService {
   
-  private String vishal_test="";
 }
